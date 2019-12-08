1412c58f9e9592a9ef3aae3d336b770e5422b025
antonS <so4usertrue@gmail.com>
2019-12-08 23:29:06 +0300

    Create app

---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>About page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, omnis!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A eius inventore labore officiis rem vitae.</p>
+  </section>
+</template>
+
+<script>
+  export default {
+    middleware: ['auth'],
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/users/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <h1>{{user.name}}</h1>
+    <hr>
+    <b>{{user.email}}</b>
+  </section>
+</template>
+
+<script>
+  export default {
+    async asyncData({params, store, error}) {
+      try {
+        const user = await store.dispatch('users/fetchUserById', params.id);
+        return {user};
+      } catch (e) {
+        error(e);
+      }
+    },
+    validate({params}) {
+      return /^\d+?$/.test(params.id);
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
